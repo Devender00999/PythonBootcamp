4117d94c46d5e2c69eec74a750ccb559358a773e
Devender Kumar <devender00999@gmail.com>
2025-06-15 22:50:00 +0530

    Added tutorial for Day12

--- Day12/Tutorial.py
+++ Day12/Tutorial.py
@@ -1,1 +1,7 @@
-# 
+# Scope is something that defines where a declared variable or function can be accessed.
+
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
