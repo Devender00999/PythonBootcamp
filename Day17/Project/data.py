9e6d82738b8cb8b3375093b450678e9630faeaf9
Devender Kumar <devender00999@gmail.com>
2025-06-23 22:22:39 +0530

    Completed Day17 of #100DaysOfCode

---
+++ Day17/Project/data.py
@@ -0,0 +1,202 @@
+question_data = [
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "The first IBM PC was released in 1981.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "A Mac is not a PC",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "hard",
+      "category": "Science: Computers",
+      "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "hard",
+      "category": "Science: Computers",
+      "question": "The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "Android versions are named in alphabetical order.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "The logo for Snapchat is a Bell.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "MacOS is based on Linux.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Ada Lovelace is often considered the first computer programmer.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+            "False"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "medium",
+      "category": "Science: Computers",
+      "question": "The open source program Redis is a relational database server.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   },
+   {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "JavaScript derives from a later version of Java",
+      "correct_answer": "False",
+      "incorrect_answers": [
+            "True"
+      ]
+   }
+]
