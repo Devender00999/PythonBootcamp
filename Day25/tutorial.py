24bc34fc3c195cc46f76ecd76a4a8f68020487c4
Devender Kumar <devender00999@gmail.com>
2025-07-10 08:28:45 +0530

    Completed tutorial for day 25.

--- Day25/tutorial.py
+++ Day25/tutorial.py
@@ -16,7 +16,7 @@ import math
 
 # reading csv file with pandas
 import pandas
-data = pandas.read_csv('weather_data.csv')
+# data = pandas.read_csv('weather_data.csv')
 
 # dataframe
 # multi dimension array in pandas
@@ -27,16 +27,35 @@ data = pandas.read_csv('weather_data.csv')
 # print( data['temp'].min())
 
 # get data in row
-print(data[data.temp == data.temp.max()])
+# print(data[data.temp == data.temp.max()])
 
 # creating datafram from scratch
+# data_dict = {
+#    "students": ["Amy", "James", "Angela"],
+#    "score": [76, 56, 65]
+# }
+
+# Saving data to csv
+# data = pandas.DataFrame(data_dict)
+# data.to_csv('new_data.csv')
+# print(data.to_json())
+
+
+
+squirrel_data = pandas.read_csv("squirrel.csv")
+gray_sq_count= len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Gray'])
+cinnamon_sq_count= len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Cinnamon'])
+black_sq_count= len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Black'])
+
+print(gray_sq_count, cinnamon_sq_count, black_sq_count)
+
 data_dict = {
-   "students": ["Amy", "James", "Angela"],
-   "score": [76, 56, 65]
+   "color": [ "gray", "cinnamon", 'black'],
+   "count": [gray_sq_count, cinnamon_sq_count, black_sq_count]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv('new_data.csv')
-# print(data.to_json())
+pandas.DataFrame(data_dict).to_csv('squirrels_count.csv')
+
+
 
 
