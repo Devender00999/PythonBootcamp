cbf73442d61a36a356c4a0bbd028a5a7ac6c73c8
Devender Kumar <devender00999@gmail.com>
2025-07-11 19:27:10 +0530

    Completed Day25 of #100DaysOfCode

---
+++ Day25/project/main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+import pandas
+
+turtle = Turtle()
+image = 'blank_states_img.gif'
+screen = Screen()
+screen.title("Name the States")
+turtle.penup()
+turtle.hideturtle()
+screen.bgpic(image)
+screen.setup(width=725, height=491)
+
+# importing data
+states = pandas.read_csv('50_states.csv')
+print(states[states['state'] == "Alabama"].to_dict())
+
+score = 0
+game_is_on = True
+while (game_is_on):
+   state = screen.textinput(f"{score}/50 Guess the state", "What's the name of the state in US").title()
+   current_state = states[states['state'] == state]
+   if len(current_state['state']) == 1:
+      x, y = current_state['x'].to_list()[0], current_state['y'].to_list()[0]
+      turtle.goto(x, y)
+      turtle.write(state.title(), font=('Times', 10, 'bold'))
+      score += 1
+
+screen.mainloop()
