ec56cd8dd07eb1679bd5256388c2efd30fdd960d
Devender Kumar <devender00999@gmail.com>
2025-07-13 18:48:59 +0530

    Startedd Day 26

--- Day25/project/main.py
+++ Day25/project/main.py
@@ -13,16 +13,31 @@ screen.setup(width=725, height=491)
 # importing data
 states = pandas.read_csv('50_states.csv')
 print(states[states['state'] == "Alabama"].to_dict())
+guessed_states = []
 
 score = 0
 game_is_on = True
 while (game_is_on):
    state = screen.textinput(f"{score}/50 Guess the state", "What's the name of the state in US").title()
+   if (state == 'Exit'):
+      missing_states = [state for state in states['state'].to_list() if state not in guessed_states]
+      # for state in states['state'].to_list():
+      #    if state not in guessed_states:
+      #       missing_states.append(state)
+      missing = pandas.DataFrame(missing_states)
+      missing.to_csv('missed_states.csv')
+      
+      
+      break
    current_state = states[states['state'] == state]
-   if len(current_state['state']) == 1:
+   if len(current_state['state']) == 1 and state not in guessed_states:
       x, y = current_state.x.item(), current_state.y.item()
       turtle.goto(x, y)
       turtle.write(state.title(), font=('Times', 10, 'bold'))
+      guessed_states.append(state)
       score += 1
+   if score == 50:
+      game_is_on = False
+   
 
 screen.mainloop()
---
+++ Day26/tutorial.py
@@ -0,0 +1,18 @@
+# list comprehension
+# creating list from a list
+# it works with any sequence for example string 
+
+# example
+numbers = [1,2,3, 4, 5, 6, 7, 8, 9, 10]
+new_list = [i + 10 for i in numbers]
+print(new_list)
+
+# with condition
+new_list = [i for i in numbers if i % 2 == 0]
+print(new_list)
+
+
+import pandas
+open('').readlines
+
+''.strip()
