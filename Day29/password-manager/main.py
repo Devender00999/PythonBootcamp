6e154c1950baba285da51d374970fed8673d7463
Devender Kumar <devender00999@gmail.com>
2025-07-22 23:05:46 +0530

    Completed Day 29 of #100DaysOfCode

--- Day29/password-manager/main.py
+++ Day29/password-manager/main.py
@@ -1,6 +1,40 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator(nr_letters, nr_symbols, nr_numbers ):
+   letters = [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+   ]
+
+   numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+   symbols = [
+   '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=',
+   '+', '[', ']', '{', '}', '\\', '|', ';', ':', '\'', '"', ',', '<',
+   '.', '>', '/', '?', '`', '~'
+   ]
+   password_list = []
+   random_letters = [random.choice(letters) for _ in range(nr_letters)]
+   random_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+   random_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+   password_list = random_letters + random_symbols + random_numbers
+   random.shuffle(password_list)
+
+   password = "".join(password_list)
+   return password
+
+def generate_password():
+   password = password_generator(8, 5, 1)
+   password_input.delete(0, END)
+   password_input.insert(0, password)
+   pyperclip.copy(password)
+   
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
@@ -57,7 +91,7 @@ password_label.grid(row=3, column=0)
 password_input = Entry(width=21)
 password_input.grid(row=3, column=1)
 
-generate_btn = Button(text="Generate Password")
+generate_btn = Button(text="Generate Password", command=generate_password)
 generate_btn.grid(row=3, column=2)
 
 # add button
--- Day5/password_generator.py
+++ Day5/password_generator.py
@@ -59,19 +59,30 @@ nr_numbers = int(input('How many numbers would you like?\n'))
 #    if (nr_symbols == 0 and symbols in li_chars):
 #       li_chars.pop(li_chars.index(symbols))
 
-password = []
-for i in range(nr_letters):
-   password += random.choice(letters)
-   
-for i in range(nr_symbols):
-   password += random.choice(symbols)
-   
-for i in range(nr_numbers):
-   password += random.choice(numbers)
+def generate_password(nr_letters, nr_symbols, nr_numbers ):
+   letters = [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+   ]
 
-random.shuffle(password)
+   numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-random_pass = ""
-for i in password:
-   random_pass += i
-print("Your password is: " + random_pass)
+   symbols = [
+   '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=',
+   '+', '[', ']', '{', '}', '\\', '|', ';', ':', '\'', '"', ',', '<',
+   '.', '>', '/', '?', '`', '~'
+   ]
+   password_list = []
+   random_letters = [random.choice(letters) for _ in range(nr_letters)]
+   random_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+   random_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+   password_list = random_letters + random_symbols + random_numbers
+   random.shuffle(password_list)
+
+   password = "".join(password_list)
+   return password
+
+print(generate_password(nr_letters, nr_symbols, nr_numbers))
