7ed4b61fe49c90859f549998e6e147f1559b9fa6
Devender Kumar <devender00999@gmail.com>
2025-08-08 23:12:50 +0530

    Completed Day 37 of #100DaysOfCode

--- Day37/main.py
+++ Day37/main.py
@@ -20,7 +20,24 @@ URL = 'https://pixe.la/v1/users'
 # print(res.reason)
 # print(res.json())
 
+headers = { "X-USER-TOKEN": TOKEN }
 # creating graph
-graph_data = { "id": "test", "name": "Graph", "unit":"commit","type":"int","color":"shibafu"}
-res = requests.post(URL + f"/{USERNAME}/graphs", json={ "id": "test", "name": "Graph", "unit":"commit","type":"int","color":"shibafu"}, headers={ "X-USER-TOKEN": TOKEN})
-print(res.text)
+# graph_data = { "id": "test", "name": "Graph", "unit":"commit","type":"int","color":"shibafu"}
+# res = requests.post(URL + f"/{USERNAME}/graphs", json={ "id": "test", "name": "Graph", "unit":"commit","type":"int","color":"shibafu"}, headers=headers)
+# print(res.text)
+
+# posting a streak on the pixela
+commit_data = { "unit": "Kms"}
+# res = requests.post(f"{URL}/{USERNAME}/graphs/test", json=commit_data, headers=headers)
+# print(res.text)
+
+
+# updating data 
+# res = requests.put(f"{URL}/{USERNAME}/graphs/test", json=commit_data, headers=headers)
+# print(res.text)
+
+# deleting data
+# res = requests.delete(f"{URL}/{USERNAME}/graphs/test", headers=headers)
+# print(res.text)
+
+
