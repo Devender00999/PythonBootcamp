693eea3bf27f8380a8a0f11de7fd92aeac3763d5
Devender Kumar <devender00999@gmail.com>
2025-08-11 22:39:20 +0530

    Added tutorial for virtual env.

---
+++ Others/tutorial.py
@@ -0,0 +1,14 @@
+# Creating an environment 
+# python -m venv environment_name
+# ex. python -m venv venv
+
+# activate an environment
+# source environment_name/bin/activate
+# ex. source venv/bin/activate
+
+# storing packages of env into a file 
+# pip3 freeze > requirements.txt
+
+# install packages from a file
+# pip3 install -r requirements.txt
+
